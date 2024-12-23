c3a97b57f62b0c876af5c1f4ac10a0dc205836e3
kloeffler95 <kevin.loeffler@colorado.edu>
2024-12-22 21:46:55 -0700

    Added logo and changed background color.

--- main.py
+++ main.py
@@ -4,23 +4,40 @@ from io import BytesIO
 from flask import Flask, jsonify
 from threading import Thread
 from kivy.app import App
+from kivy.graphics import Color, Rectangle
 from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.core.image import Image as CoreImage
 from kivy.clock import Clock
+from kivy.config import Config
+from kivy.core.window import Window
 
 app = Flask(__name__)
 
 class RefurboardApp(App):
     def build(self):
+        self.icon = 'assets/logo.png'  # Set the application icon
         self.layout = BoxLayout(orientation='vertical')
-        self.label = Label(text='Starting HTTP server...')
+        self.layout.canvas.before.clear()
+        
+        # Add the logo image
+        self.logo = Image(source='assets/logo.png', size_hint=(None, None), size=(200, 200), pos_hint={'center_x': 0.5, 'top': 1})
+        self.layout.add_widget(self.logo)
+        
+        self.label = Label(text='Starting HTTP server...', color=(0, 0, 0, 1))  # Set label text color to black
         self.layout.add_widget(self.label)
         self.qr_image = Image()
         self.layout.add_widget(self.qr_image)
         Clock.schedule_once(self.start_server, 1)
+        with self.layout.canvas.before:
+            Color(1, 1, 1, 1)  # Set the background color to white
+            self.rect = Rectangle(size=Window.size, pos=self.layout.pos)
+            self.layout.bind(size=self._update_rect, pos=self._update_rect)
         return self.layout
+
+    def _update_rect(self, instance, _):
+        self.rect.size = instance.size
 
     def get_ip_address(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,7 +51,7 @@ class RefurboardApp(App):
             s.close()
         return IP
 
-    def start_server(self, dt):
+    def start_server(self, _):
         ip_address = self.get_ip_address()
         self.label.text = f"HTTP server running at http://{ip_address}:5000"
         self.generate_qr_code(f"http://{ip_address}:5000")
