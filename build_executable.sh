d268a8d5fb1c8d50e0e1c0cff44b01e10c2059c0
kloeffler95 <kevin.loeffler@colorado.edu>
2025-01-02 22:13:33 -0700

    Adding gitignore.

--- build_executable.sh
+++ build_executable.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Ensure the script is executable
-chmod +x build_executable.sh
-
 # Run PyInstaller with the spec file
-pyinstaller refurboard.spec
+poetry run pyinstaller refurboard.spec
